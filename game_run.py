566fe1eef9767638ea059ae608b49f96cb78d89e
Kaiwen <zkaiwen98@gmail.com>
2023-10-28 00:42:57 +0100

    Change the format

--- game_run.py
+++ game_run.py
@@ -6,16 +6,16 @@ from random_agent import random_policy
 def game_run(model):
     state = model.state
     player = 1
-    # player = random.choice(state.players)
     while model.state.is_terminal() is None:
         action = random_policy(state.cards)
-        print('player'+ str(player) + 'suggests'+ str(action))
+        print('Player {} suggests {}'.format(player, action))
         state.suggest(player,action)
+        state.step += 1
         state.if_accuse(player)
         player = state.next_player(player)
     if model.state.is_terminal() is not None:
-        print("winner is " + str(model.state.is_terminal()))
+        print('In the step {}, the winner is player {}'.format(state.step, model.state.is_terminal()))
         return model.state.is_terminal()
-6
+
 if __name__ == "__main__":
     game_run(GameModel(3))
